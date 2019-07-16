66528ee3ce201fb7cca9df25c3e17afa9a10ddd1
gayathri37 <52819299+gayathri37@users.noreply.github.com>
2019-07-16 15:16:34 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+y = int(input("Enter a year: "))  
+if (y % 4) == 0:  
+    if (y % 100) == 0:
+        if (y % 400) == 0:  
+               print(y,"is a leap year") 
+        else:  
+            print(y,"is not a leap year")  
+    else:  
+        print(y,"is a leap year")  
+else:  
+    print(y,"is not a leap year")  
